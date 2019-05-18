2729fb24673a1f2aa6e7d5b69e63d47abaf010f8
Krzysztof Jopek <krzysztofjopek99@gmail.com>
2019-05-18 12:08:51 +0200

    Add ERROR macro

--- src/global.h
+++ src/global.h
@@ -5,7 +5,7 @@
 #include <stdlib.h>
 #include <SDL2/SDL.h>
 
-#define ERROR();
+#define ERROR() do{fprintf(stderr, "ERROR: %s", SDL_GetError()); exit(1);}while(0)
 
 extern SDL_Renderer* g_renderer;
 
